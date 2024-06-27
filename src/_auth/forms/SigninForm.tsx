b41e69080bd9af2d7c9a05cef820ce3ca51bb44e
yash <injose.joshi@gmail.com>
2024-06-28 03:20:54 +0530

    Add cosmetic refactoring

--- src/_auth/forms/SigninForm.tsx
+++ src/_auth/forms/SigninForm.tsx
@@ -15,7 +15,6 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import Loader from "@/components/shared/Loader";
 import { useToast } from "@/components/ui/use-toast";
 
 import { SigninValidation } from "@/lib/validation";
--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -13,7 +13,6 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import Loader from "@/components/shared/Loader";
 import { useToast } from "@/components/ui/use-toast";
 
 import {
